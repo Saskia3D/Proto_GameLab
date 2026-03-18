a1d6038c9b35332886e07ca7a796d5d33d58a961
Saskia Dessapes <91622318+Saskia3D@users.noreply.github.com>
2026-03-17 22:26:21 -0400

    Changement du trackspline pour les assets 2d

--- Source/ProtoGameLab/TrackSplineActor.cpp
+++ Source/ProtoGameLab/TrackSplineActor.cpp
@@ -1,13 +1,9 @@
-/// TrackSplineActor.cpp
-
 #include "TrackSplineActor.h"
 #include "Components/SplineComponent.h"
 #include "Components/SplineMeshComponent.h"
 
-// Sets default values
 ATrackSplineActor::ATrackSplineActor()
 {
-	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = false;
 
 	Spline = CreateDefaultSubobject<USplineComponent>(TEXT("Spline"));
@@ -21,12 +17,6 @@ ATrackSplineActor::ATrackSplineActor()
 void ATrackSplineActor::OnConstruction(const FTransform& Transform)
 {
 	Super::OnConstruction(Transform);
-	UE_LOG(LogTemp, Warning, TEXT("TrackSplineActor OnConstruction called"));
-
-	Spline->SetClosedLoop(bClosedLoop);
-
-	Spline->UpdateSpline();
-
 	ClearGenerated();
 	BuildRoad();
 }
@@ -34,69 +24,91 @@ void ATrackSplineActor::OnConstruction(const FTransform& Transform)
 
 void ATrackSplineActor::ClearGenerated()
 {
-	for (USplineMeshComponent* C : RoadSegments)
+	for (USplineMeshComponent* Seg : RoadSegments)
 	{
-		if (C) C->DestroyComponent();
+		if (Seg) Seg->DestroyComponent();
 	}
 	RoadSegments.Reset();
+
+	for (USplineMeshComponent* Seg : SpriteSegments)
+	{
+		if (Seg) Seg->DestroyComponent();
+	}
+	SpriteSegments.Reset();
 }
 
 void ATrackSplineActor::BuildRoad()
 {
-	if (!RoadMesh || !Spline) return;
+	if (!Spline || !RoadMesh || !SpriteMesh) return;
 
-	const int32 NumPoints = Spline->GetNumberOfSplinePoints();
-	if (NumPoints < 2) return;
+	float SplineLength = Spline->GetSplineLength();
+	float CurrentDistance = 0.f;
 
-	auto MakeSeg = [&](int32 A, int32 B)
-		{
-			USplineMeshComponent* Seg = NewObject<USplineMeshComponent>(this);
-			Seg->SetMobility(EComponentMobility::Movable);
-			Seg->SetStartScale(FVector2D(6.f, 0.05f));
-			Seg->SetEndScale(FVector2D(6.f, 0.05f));
-			Seg->RegisterComponentWithWorld(GetWorld());
-			Seg->AttachToComponent(Spline, FAttachmentTransformRules::KeepRelativeTransform);
-
-			Seg->SetStaticMesh(RoadMesh);
-			if (RoadMaterial) Seg->SetMaterial(0, RoadMaterial);
-
-			const FVector StartPos = Spline->GetLocationAtSplinePoint(A, ESplineCoordinateSpace::Local);
-			const FVector StartTan = Spline->GetTangentAtSplinePoint(A, ESplineCoordinateSpace::Local);
-			const FVector EndPos = Spline->GetLocationAtSplinePoint(B, ESplineCoordinateSpace::Local);
-			const FVector EndTan = Spline->GetTangentAtSplinePoint(B, ESplineCoordinateSpace::Local);
-
-			Seg->SetStartAndEnd(StartPos, StartTan, EndPos, EndTan, true);
-			Seg->SetForwardAxis(ESplineMeshAxis::X);
-
-			Seg->SetCollisionEnabled(bCollisionEnabled ? ECollisionEnabled::QueryAndPhysics : ECollisionEnabled::NoCollision);
-			Seg->SetCollisionProfileName(TEXT("BlockAll"));
-
-			Seg->SetStartScale(FVector2D(RoadWidthScale, 0.05f));
-			Seg->SetEndScale(FVector2D(RoadWidthScale, 0.05f));
-
-			RoadSegments.Add(Seg);
-		};
-
-	for (int32 i = 0; i < NumPoints - 1; ++i)
+	while (CurrentDistance < SplineLength)
 	{
-		MakeSeg(i, i + 1);
-	}
+		float NextDistance = FMath::Min(CurrentDistance + TileLength, SplineLength);
 
-	if (bClosedLoop)
-	{
-		MakeSeg(NumPoints - 1, 0);
+		FVector StartPos = Spline->GetLocationAtDistanceAlongSpline(CurrentDistance, ESplineCoordinateSpace::Local);
+		FVector EndPos = Spline->GetLocationAtDistanceAlongSpline(NextDistance, ESplineCoordinateSpace::Local);
+
+		FVector StartTan = Spline->GetDirectionAtDistanceAlongSpline(CurrentDistance, ESplineCoordinateSpace::Local) * TileLength;
+		FVector EndTan = Spline->GetDirectionAtDistanceAlongSpline(NextDistance, ESplineCoordinateSpace::Local) * TileLength;
+
+		// Road base
+		USplineMeshComponent* RoadSeg = NewObject<USplineMeshComponent>(this);
+		RoadSeg->SetFlags(RF_Transactional);// Enable track changes
+		RoadSeg->SetMobility(EComponentMobility::Movable);
+		RoadSeg->RegisterComponentWithWorld(GetWorld());
+		RoadSeg->AttachToComponent(Spline, FAttachmentTransformRules::KeepRelativeTransform);
+		RoadSeg->SetStaticMesh(RoadMesh);
+		if (RoadMaterial) RoadSeg->SetMaterial(0, RoadMaterial);
+		RoadSeg->SetStartAndEnd(StartPos, StartTan, EndPos, EndTan, true);
+		RoadSeg->SetStartScale(FVector2D(RoadWidthScale, 0.05f));
+		RoadSeg->SetEndScale(FVector2D(RoadWidthScale, 0.05f));
+		RoadSeg->SetForwardAxis(ESplineMeshAxis::Y);
+		RoadSegments.Add(RoadSeg);
+
+		// Sprite top
+		USplineMeshComponent* SpriteSeg = NewObject<USplineMeshComponent>(this);
+		SpriteSeg->SetMobility(EComponentMobility::Movable);
+		SpriteSeg->TranslucencySortPriority = 1;
+		SpriteSeg->RegisterComponentWithWorld(GetWorld());
+		SpriteSeg->AttachToComponent(Spline, FAttachmentTransformRules::KeepRelativeTransform);
+		SpriteSeg->SetStaticMesh(SpriteMesh);
+		SpriteSeg->SetRelativeRotation(FRotator(0.f, 0.f, 0.f));
+		if (SpriteMaterial) SpriteSeg->SetMaterial(0, SpriteMaterial);
+
+		FVector SpriteStart = StartPos + FVector(0, 0, SpriteHeightOffset);
+		FVector SpriteEnd = EndPos + FVector(0, 0, SpriteHeightOffset);
+		float SpriteWidth = RoadWidthScale * 0.4f;
+
+		SpriteSeg->SetStartAndEnd(SpriteStart, StartTan, SpriteEnd, EndTan, true);
+		SpriteSeg->SetStartScale(FVector2D(SpriteWidth, 0.05f));
+		SpriteSeg->SetEndScale(FVector2D(SpriteWidth, 0.05f));
+		SpriteSeg->SetForwardAxis(ESplineMeshAxis::Y);
+		SpriteSeg->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+		SpriteSegments.Add(SpriteSeg);
+
+		CurrentDistance = NextDistance;
+		if (TileLength <= 0.f) break;
 	}
 }
+#if WITH_EDITOR
+void ATrackSplineActor::PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent)
+{
+	Super::PostEditChangeProperty(PropertyChangedEvent);
 
-// Called when the game starts or when spawned
+	// Only rebuild if we changed a track-related property
+	BuildRoad();
+}
+#endif
+
 void ATrackSplineActor::BeginPlay()
 {
 	Super::BeginPlay();
 }
 
-// Called every frame
 void ATrackSplineActor::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 }
-
--- Source/ProtoGameLab/TrackSplineActor.h
+++ Source/ProtoGameLab/TrackSplineActor.h
@@ -1,4 +1,3 @@
-// TrackSplineActor.h
 
 #pragma once
 
@@ -16,42 +15,63 @@ class PROTOGAMELAB_API ATrackSplineActor : public AActor
 	GENERATED_BODY()
 
 public:
-	// Sets default values for this actor's properties
 	ATrackSplineActor();
 
 #if WITH_EDITOR
 	virtual void OnConstruction(const FTransform& Transform) override;
 #endif
 
-	// Route
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Track")
+	// --- EXISTING ROAD SETTINGS ---
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Track")
 	TObjectPtr<USplineComponent> Spline;
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Track|Road")
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Track|Road")
 	TObjectPtr<UStaticMesh> RoadMesh = nullptr;
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Track|Road")
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Track|Road")
 	TObjectPtr<UMaterialInterface> RoadMaterial = nullptr;
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Track|Road")
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Track|Road")
 	bool bClosedLoop = true;
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Track|Road")
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Track|Road")
 	bool bCollisionEnabled = true;
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Track|Road")
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Track|Road")
 	float RoadWidthScale = 1.0f;
 
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Track|Sprite")
+	float SpriteWidthScale = 0.5f;
+
+	// --- NEW SPRITE SETTINGS ---
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Track|Sprite")
+	TObjectPtr<UStaticMesh> SpriteMesh = nullptr;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Track|Sprite")
+	TObjectPtr<UMaterialInterface> SpriteMaterial = nullptr; // The road texture material
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Track|Sprite")
+	float SpriteHeightOffset = 3.0f; // Lifts the sprite slightly above the road to prevent flickering
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Track|Sprite")
+	float TileLength = 400.0f; // The physical length of one road tile in Unreal Units
+
+#if WITH_EDITOR
+	virtual void PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent) override;
+#endif
+
 protected:
-	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
 public:
-	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
+	// --- SEGMENT ARRAYS ---
 	UPROPERTY(Transient)
 	TArray<TObjectPtr<USplineMeshComponent>> RoadSegments;
+
+	UPROPERTY(Transient)
+	TArray<TObjectPtr<USplineMeshComponent>> SpriteSegments; // Track the sprite layer separately
 
 	void ClearGenerated();
 	void BuildRoad();
