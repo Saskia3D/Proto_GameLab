54fc20de3c3b53952725305d2bafc8fc80d2ea36
kouakan.assemian_ange_uriel <kouakan.assemian_ange_uriel@courrier.uqam.ca>
2026-04-04 16:43:45 -0400

    Correction vitesse du drift

--- Source/ProtoGameLab/STR_RacerPawn.h
+++ Source/ProtoGameLab/STR_RacerPawn.h
@@ -288,13 +288,13 @@ protected:
 	float NormalGrip = 17.f;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement|Drift")
-	float DriftGrip = 12.f;
+	float DriftGrip = 14.f;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement|Drift")
-	float DriftSpeedLossPerSecond = 100.f;
+	float DriftSpeedLossPerSecond = 15.f;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement|Drift")
-	float DriftAccelMultiplier = 0.3f;
+	float DriftAccelMultiplier = 1.5f;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement|Drift")
 	float DriftDirectionSwitchThreshold = 0.7f;
